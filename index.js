62296c7396589f1331d079a2aa566d2b3d023835
Carlos Eduardo <eduardo938soares@gmail.com>
2026-09-20 18:35:52 -0300

    Refactor item checks and add console messages
    Updated item checks and added new conditions for optional items, documents, and food. Enhanced console messages for clarity.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+
+//---Lista de equipamentos basicos para 5km na sua cidade---
 const tenis = true;
 const agua = true;
-const suplemento= false;
+const caixaDeSom = false;
 const celular = true;
-const documento = false;
+const documento = true;
+const dinheiro = false;
+const alimento = false;
 
 // Verificar se os itens básicos estão disponíveis
 if (tenis && agua) {
@@ -12,15 +16,58 @@ if (tenis && agua) {
 }
 
 // Verificar itens opcionais
-if (suplemento || celular || documento) {
-  console.log("Você também tem pelo menos um item extra.");
+if ( caixaDeSom || celular || documento ) {
+  console.log("esta presente pelo menos 1 item opicional");
 } else {
   console.log("Você não tem nenhum item extra.");
 }
 
+// verificando itens que não precisa
+if (caixaDeSom){
+  console.log("item é necessario")
+} else {
+  console.log("item não necessario")
+}
+
 // Verificar se pode participar da corrida
-if (tenis && (documento || celular)) {
-  console.log("Você está pronto para participar da corrida!");
+if (tenis &&  documento || (celular)) {
+  console.log("Você está quase pronto!");
 } else {
   console.log("Leve seu tênis e documento ou celular antes de sair.");
 }
+
+if (documento) {
+  console.log("não esqueça documento")
+} else{
+  console.log("está tudo ok com os documentos")
+}
+if (tenis) {
+  console.log("tenis está ok")
+} else{
+  console.log("não esqueça do tenis")
+}
+if(celular) {
+  console.log("celular está ok")
+} else{
+  console.log("Não esqueça do celular")
+} 
+
+// verificar alimentação
+if (dinheiro || alimento) {
+  console.log("você tem algum dinheiro ou alimento")
+} else{
+  console.log("leve dinheiro ou alimento")
+}
+
+//resumo de tudo
+const resumo = `
+tenis: ${tenis}
+agua: ${agua}
+caixaDeSom: ${caixaDeSom}
+celular: ${celular}
+documento: ${documento}
+dinheiro: ${dinheiro}
+alimento: ${alimento}
+`;
+
+console.log(resumo);
